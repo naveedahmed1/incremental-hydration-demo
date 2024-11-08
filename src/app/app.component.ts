3dbf6139c68e4500f1d8621714119cad9d277450
Naveed Ahmed <naveed@technobird.com>
2024-11-09 02:14:26 +0500

    @angular/core migration - explicit-standalone-flag
    Updates non-standalone Directives, Component and Pipes to 'standalone:false' and removes 'standalone:true' from those who are standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavLinksComponent } from './nav-links/nav-links.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    NavLinksComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        NavLinksComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'temp';
--- src/app/nav-links/nav-links.component.ts
+++ src/app/nav-links/nav-links.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-links',
-  standalone: true,
-  imports: [
-    RouterLink
-  ],
-  templateUrl: './nav-links.component.html',
-  styleUrl: './nav-links.component.scss'
+    selector: 'app-nav-links',
+    imports: [
+        RouterLink
+    ],
+    templateUrl: './nav-links.component.html',
+    styleUrl: './nav-links.component.scss'
 })
 export class NavLinksComponent {
 
